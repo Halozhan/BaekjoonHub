d51c07b93509395a12656d8b75147d868d699c78
Halozhan <juwonhwang0411@gmail.com>
2023-03-27 23:36:59 +0900

    Update [Bronze III] Title: 곱셈

--- 백준/Bronze/2588. 곱셈/곱셈.java
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -7,8 +7,8 @@ public class Main
         int b = scan.nextInt();
         scan.close();
         
-        System.out.println(a*(b%(b/10*10)));
-        System.out.println(a*((b%(b/100*100))/10));
+        System.out.println(a*(b%10));
+        System.out.println(a*((b%100) / 10));
         System.out.println(a*(b/100));
         System.out.println(a*b);
     }
