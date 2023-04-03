c8565bb5c106a6780b91a38acf6e9b517cf144f1
Halozhan <juwonhwang0411@gmail.com>
2023-04-03 17:20:13 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 316 ms, Memory: 18088 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int lines = scan.nextInt();
+        scan.close();
+        for(int i = 0; i < lines; i++) {
+            for(int j = 0; j < i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
