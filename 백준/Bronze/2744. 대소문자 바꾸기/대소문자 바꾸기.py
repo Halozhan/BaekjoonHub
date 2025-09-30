2fd85d16c1dd1bc13a09748036721df9c0efc57b
Juwon Hwang <juwonhwang0411@gmail.com>
2025-09-30 14:26:55 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.py
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.py
@@ -1,7 +1,1 @@
-text = str(input())
-for i in text:
-    if i.islower():
-        temp = i.upper()
-    elif i.isupper():
-        temp = i.lower()
-    print(temp, end="")
+print(input().swapcase())
